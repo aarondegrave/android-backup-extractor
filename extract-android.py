d79b1fbb26f424885ecf783eda8768c9754ca0e3
aarondegrave <54682537+aarondegrave@users.noreply.github.com>
2020-10-20 09:24:14 -0400

    Add files via upload
    Argument handling for chunk size...

--- extract-android.py
+++ extract-android.py
@@ -7,16 +7,20 @@ import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file", help = "Point me to the file", type=str)
+parser.add_argument("--chunk", help="enter int chunk size in bytes", type=int)
 args = parser.parse_args()
 
 with open(args.file, 'rb') as f:
     b = 0
     for data in f:
-        n= 4096
-        b = b + 1
-        f.seek(24)
-        data= f.read(n*b)
-        continue
+        if args.chunk:
+            b = b + 1
+            f.seek(24)
+            data= f.read(args.chunk*b)
+            continue
+        else:
+            f.seek(24)
+            data=f.read()
     stream = zlib.decompress(data)
     tar = tarfile.open(fileobj=io.BytesIO(stream))
     members = tar.getmembers()
